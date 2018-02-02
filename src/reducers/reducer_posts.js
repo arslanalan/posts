aafe8b00511c3754608b3b4ad30f8fb65b0b560b
Arslan ALAN <alan.arslan@gmail.com>
2018-02-02 10:35:58 +0300

    Created reducer_posts for action fetchPosts

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import _ from 'lodas';
+import { FETCH_POSTS } from '../actions';
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            //console.log(action.payload.data); //expect [post1, post2 ]
+            //need transformation to { 4: post }
+            //lodash library has a method doing exactly the same thing
+            return _.mapKeys(action.payload.data, 'id');
+
+        default:
+            return state;
+    }
+}
