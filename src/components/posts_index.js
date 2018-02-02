88adbcbbf78216e66f344d823f6d7f4ea5224b9b
Arslan ALAN <alan.arslan@gmail.com>
2018-02-02 11:46:20 +0300

    Functionality: list of posts shown on page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,48 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+    componentDidMount() {
+        //wait for data loading
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        //this.props.posts.map is not possible, because this.props.posts is an object, not array
+        //So, we'll use lodash's _.map method
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
     render() {
+        console.log(this.props.posts);
         return (
             <div>
-                Post Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default PostIndex;
+//Remember when everyone to consume anything from application level state
+//we always define our map state to prop's function
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
+//mapDispatchToProps() function is not used, it's not required in most cases
+//Just in some cases it's needed like doing some computing
+//function mapDispatchToProps(dispatch) {
+//    return bindActionCreators({ fetchPosts }, dispatch);
+//}
