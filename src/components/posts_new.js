1f676d5b14203369a0e6d57739f4a61bc33281fa
Arslan ALAN <alan.arslan@gmail.com>
2018-02-05 10:12:23 +0300

    Validate forms and show errors to users

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,6 +11,7 @@ class PostsNew extends Component {
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         );
     }
@@ -24,8 +25,8 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -38,6 +39,29 @@ class PostsNew extends Component {
     }
 }
 
+//Automatically called on form submit
+function validate(values) {
+    // console.log(values) -> { title: 'dfdsfsa', categories: 'fgbfbfgb', content: 'vvdfmv kfşlm şlşdsmc'}
+    const errors = {};
+
+    // Validate the inputs from 'values'
+    //title, categories and content is specific key for the errors object. These keys are the same with the name of the related field.
+    if (!values.title) {
+        errors.title = 'Enter a title';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content please';
+    }
+
+    //If return empty errors object, redux form assumes no error, everything is OK with that form
+    //If errors has *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,  //same as validate: validate
     form: 'PostsNewForm'  //this name must be unique
 })(PostsNew);
